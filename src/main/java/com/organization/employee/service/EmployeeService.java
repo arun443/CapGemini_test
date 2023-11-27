23c5dafbcc9b6e3e3eb471395f89a308017fe087
arun443 <98135707+arun443@users.noreply.github.com>
2023-11-27 17:04:12 +0000

    Add files via upload

---
+++ src/main/java/com/organization/employee/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.organization.employee.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+	
+	@Autowired
+	MessageSource messages;
+
+	
+}
